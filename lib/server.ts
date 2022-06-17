9466a1aeac27bd18fd081fcb8c0da1835d942e8f
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-06-17 10:43:48 -0500

    Finished project

--- lib/server.ts
+++ lib/server.ts
@@ -8,7 +8,6 @@ import authorRouter from './routes/Author.routes';
 const app = express();
 app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/user', userRouter)
 app.use('/post', postRouter)
